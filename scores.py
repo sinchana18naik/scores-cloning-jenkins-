4cc10999e07b203f8963130d6fe290c9670d0d5d
sinchana18naik <sinchananaik112005@gmail.com>
2025-11-28 08:26:58 +0530

    save to new branch

--- scores.py
+++ scores.py
@@ -7,15 +7,18 @@ else:
     
     scores = [50, 60, 70, 80, 90]
     print("No input given - using default scores:")
+ 
 
 
 total = sum(scores)
 average = total / len(scores)
-
-
+min_score = min(scores)
+max_score = max(scores)
 
 print(f"Scores: {scores}")
 print(f"Sum of scores: {total}")
 print(f"Average of scores: {average:.2f}")
+print("Minimum Score:", min_score)
+print("Maximum Score:", max_score) ##
 
   
